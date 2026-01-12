e81d22b69a005dee2971d22e6b33322311b9bb59
kmralok795 <kr7908alok@gmail.com>
2026-01-13 04:55:33 +0530

    Create stack2.java

---
+++ stack2.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+class stackB{
+  static class Node{
+    int data;
+    Node next;
+    Node(int data){
+      this.data=data;
+      this.next=null;
+    }
+  }
+  
+  static class Stack{
+    static Node head=null;
+    
+    
+    public static boolean isEmpty(){
+      return head==null;
+    }
+    
+    public static void push(int data){
+      Node newNode=new Node(data);
+      if(isEmpty()){
+        head=newNode;
+        return;
+      }
+      newNode.next=head;
+      head=newNode;
+    } 
+    
+    
+    public static int pop(){
+      if(isEmpty()){
+        return -1;
+      }
+      int top=head.data;
+      head=head.next;
+      return top;
+    }
+  
+  
+    public static int peek(){
+      if(isEmpty()){
+        return -1;
+      }
+      return head.data;
+    }
+  }
+  
+  
+  
+  
+  public static void main(String ar[]){
+    Stack s=new Stack();
+    s.push(1);
+    s.push(3);
+    s.push(3);
+    s.push(7);
+    s.push(7);
+    s.push(9);
+    s.push(11);
+    
+    
+    
+    System.out.println("Stack is empty: "+s.isEmpty());
+    while(!s.isEmpty()){
+      System.out.println(s.peek());
+      s.pop();
+    }
+    
+    System.out.println("Stack is empty: "+s.isEmpty());
+  }
+}
